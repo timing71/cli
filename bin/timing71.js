b510ea61ec2e830c9283c575bff713d2bd06418c
James Muscat <jamesremuscat@gmail.com>
2023-06-10 17:11:02 +0100

    Make sure `EventSource` is available globally.
    Should probably do this through `ConnectionService` but this works for
    now.

--- bin/timing71.js
+++ bin/timing71.js
@@ -5,6 +5,9 @@ import { ReadableStream, TransformStream } from 'web-streams-polyfill/dist/ponyf
 global.ReadableStream = ReadableStream;
 global.TransformStream = TransformStream;
 
+import EventSource from 'eventsource';
+global.EventSource = EventSource;
+
 // use async import so the polyfill above takes effect before @zip.js gets imported
 import('../src/index.js').then(
   ({ main }) => main(process.argv)
