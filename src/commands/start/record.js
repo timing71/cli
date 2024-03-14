2ea929213f5948dc644128a3ad65c9e75e2d35fe
James Muscat <jamesremuscat@gmail.com>
2024-03-14 17:07:53 +0000

    Rewrite manifest if start time is reset.

--- src/commands/start/record.js
+++ src/commands/start/record.js
@@ -4,6 +4,7 @@ export class Recorder {
   constructor(uuid) {
     this.uuid = uuid;
     this._startTime = null;
+    this._manifest = null;
   }
 
   get outputDirectory() {
@@ -21,18 +22,22 @@ export class Recorder {
     this._writeFile(filename, state);
     if (!this._startTime && state.lastUpdated) {
       this._startTime = Math.floor(state.lastUpdated / 1000);
+      console.log(`Set start time of recording to ${this._startTime}`);
+      if (this._manifest) {
+        // Rewrite the manifest if we've already written it
+        this.writeManifest(this._manifest);
+      }
     }
   }
 
   writeManifest(manifest) {
-    this._writeFile(
-      'manifest.json',
-      {
-        ...manifest,
-        startTime: this._startTime || manifest.startTime,
-        version: 1
-      }
-    );
+    this._manifest = {
+      ...manifest,
+      startTime: this._startTime || manifest.startTime,
+      version: 1
+    };
+
+    this._writeFile('manifest.json', fullManifest);
   }
 
   _writeFile(filename, data) {
